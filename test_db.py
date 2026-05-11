a8ef13287d5d0fe590b46c5839bda50adc1c652c
farelrunin <fhariel28@gmail.com>
2026-05-11 17:16:40 +0700

    chore: hapus sisa-sisa file Render, Vercel, dan Python untuk full Railway

--- test_db.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-try:
-    conn = pymysql.connect(host='localhost', user='root', password='', database='nutriai_db', connect_timeout=5)
-    print('MySQL Connection OK')
-    conn.close()
-except Exception as e:
-    print(f'MySQL Error: {e}')
